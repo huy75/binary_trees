f3e3302c49216a59e742b5c42aee8e5f2203dc9b
huy75 <huy.nguyen66@gmail.com>
2021-03-08 16:07:21 +0000

    feat is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - check if a binary tree is full
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if full, 0 if NULL or not full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int hl, hr;
+
+	if (!tree)
+		return (0);
+
+	hl = tree->left ? 1 + binary_tree_is_full(tree->left) : 0;
+	hr = tree->right ? 1 + binary_tree_is_full(tree->right) : 0;
+	return (hl == hr);
+}
